d3e3efb8d4bc89a5a2aad53f6a761a3bbafc992a
Ani Rogava <80489873+anirogava@users.noreply.github.com>
2022-04-04 23:24:54 +0400

    basic json

--- 2-Node-Express/final/6-basic-json.js
+++ 2-Node-Express/final/6-basic-json.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const app = express();
+const { products } = require("./data");
 
 app.get("/", (req, res) => {
-  res.json([{ name: "John" }, { name: "Susan" }]);
+  res.json(products);
 });
 
 app.listen(5000, () => {
