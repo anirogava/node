f4ff0fd966f34c4839ad7663acdfcd1a4f503ddf
Ani Rogava <80489873+anirogava@users.noreply.github.com>
2022-04-05 19:08:44 +0400

    controllers

--- 2-Node-Express/routes/people.js
+++ 2-Node-Express/routes/people.js
@@ -7,12 +7,15 @@ const {
   updatePerson,
   deletePerson,
 } = require("../controllers/people");
-router.get("/", getPeople);
 
-router.post("/", createPerson);
-router.post("/postman", createPersonPostman);
-router.put("/:id", updatePerson);
+// router.get("/", getPeople);
+// router.post("/", createPerson);
+// router.post("/postman", createPersonPostman);
+// router.put("/:id", updatePerson);
+// router.delete("/:id", deletePerson);
 
-router.delete("/:id", deletePerson);
+router.route("/").get(getPeople).post(createPerson);
+router.route("/postman").post(createPersonPostman);
+router.route("/:id").put(updatePerson).delete(deletePerson);
 
 module.exports = router;
